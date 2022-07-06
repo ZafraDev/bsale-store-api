59590040d20a9b27d78e3af33e5d69c7473075d4
Rodrigo Berrios <rodrigoberrios1998@gmail.com>
2022-07-05 22:05:59 -0500

    feat: search filter added to getProducts function

--- src/services/getProductsByCategory.js
+++ src/services/getProductsByCategory.js
@@ -3,18 +3,20 @@ const { response } = require("../utils/response");
 
 exports.handler = async (event, context) => {
   const { categoryId } = event.pathParameters;
+  const { search } = event.queryStringParameters
   let results;
   if (categoryId === "0")
     results = await db.query(
       `SELECT p.id, p.name, p.url_image, p.price, p.discount
-      FROM product p`
+      FROM product p WHERE p.name LIKE ?`,
+      ['%' + search + '%']
     );
   else
     results = await db.query(
       `SELECT p.id, p.name, p.url_image, p.price, p.discount
       FROM product p
-      WHERE p.category = ?`,
-      [categoryId]
+      WHERE p.category = ? AND p.name LIKE ?`,
+      [categoryId, '%' + search + '%']
     );
 
   db.end();
