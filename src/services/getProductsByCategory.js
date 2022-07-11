b97290f9654bf97319cdeb3f4abcc224ff4e4b50
Rodrigo Berrios <rodrigoberrios1998@gmail.com>
2022-07-10 22:00:06 -0500

    fix: errors when send caracters in limit, page and categoryID params

--- src/services/getProductsByCategory.js
+++ src/services/getProductsByCategory.js
@@ -5,8 +5,9 @@ exports.handler = handleStructure(async (event, context) => {
   const { categoryId } = event.pathParameters;
   const { search, limit, page } = { ...event.queryStringParameters };
 
-  const LIMIT = limit ? parseInt(limit) : 10;
-  const PAGE = page ? parseInt(page) : 1;
+  const CATEGORY = parseInt(categoryId) || 0;
+  const LIMIT = limit ? parseInt(limit) || 10 : 10;
+  const PAGE = page ? parseInt(page) || 1 : 1;
   const OFFSET = (PAGE - 1) * LIMIT;
 
   const paginationClause = ` LIMIT ${LIMIT} OFFSET ${OFFSET}`;
@@ -15,8 +16,8 @@ exports.handler = handleStructure(async (event, context) => {
     "SELECT p.id, p.name, p.url_image, p.price, p.discount FROM product p";
   let totalQuery = "SELECT COUNT(*) as total FROM product p";
 
-  if (categoryId !== "0") {
-    let whereClause = ` WHERE p.category = ${categoryId}`;
+  if (CATEGORY !== 0) {
+    let whereClause = ` WHERE p.category = ${CATEGORY}`;
     if (search) whereClause += ` AND p.name LIKE '%${search}%'`;
     query += whereClause;
     totalQuery += whereClause;
